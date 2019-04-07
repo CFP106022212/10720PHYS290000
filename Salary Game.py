1487c30597fe1694142cc0abd34accd32dbe24f6
CFP106022212 <48315247+CFP106022212@users.noreply.github.com>
2019-04-07 17:25:49 +0800

    Update Salary Game.py

--- Salary Game.py
+++ Salary Game.py
@@ -10,7 +10,7 @@ n    = 1000   #students number
 m    = 10     #money
 dm   = 3      #paid
 k    = 1000   #game times
-stop = True   #whether drop when money is 0 
+stop = False  #whether drop when money is 0 
 a    = list(range(0,n))
 students = np.zeros(n)+m
 def select():
@@ -22,17 +22,18 @@ for i in range(0,k):
     if stop :
         while students[player[0]]<= 0 or students[player[1]]<=0:
             player = select()
-    while True:
-        result = np.random.randint(3)
-        if   result == 0:
-            students[player[0]]+=dm
-            students[player[1]]-=dm
-            break
-        elif result == 1:
-            students[player[0]]-=dm
-            students[player[1]]+=dm
-            break
+    
+    result = np.random.randint(3) #猜拳
+    if   result == 0:
+        students[player[0]]+=dm
+        students[player[1]]-=dm
+    elif result == 1:
+        students[player[0]]-=dm
+        students[player[1]]+=dm
+        
 import matplotlib.pyplot as plt
 plt.hist(students)
 plt.show()
+
+
 
